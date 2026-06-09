38148b5ae7c3eb8c8012094bb7ce22d0d8264baa
SheckHall <13789176472@163.com>
2026-06-09 11:56:34 +0800

    Clean up main.go by removing unused server code
    Removed commented-out code for HTTP/HTTPS server startup.

--- main.go
+++ main.go
@@ -15,27 +15,6 @@ func main() {
 
 	r := router.InitRouter()
 
-	// 校园网启动
-	//crtPath := "ssl/server.crt"
-	//keyPath := "ssl/server.key"
-	//_, errC := os.Stat(crtPath)
-	//_, errK := os.Stat(keyPath)
-	//
-	//if errC != nil || errK != nil {
-	//	log.Println("启用HTTP")
-	//	err := r.Run("localhost:8080") // http启动
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//} else { // https启动
-	//	log.Println("启用HTTPS")
-	//	err := r.RunTLS("10.17.42.43:8443", crtPath, keyPath)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
-
 	// RadminLan启动
 	lcrtPath := "ssl/Radmin_LAN/server_LAN.crt"
 	lkeyPath := "ssl/Radmin_LAN/server_LAN.key"
